b31c1e5cafbf68252ad65d53bfd4c336fb605182
yuchiko <iyuchiko@gmail.com>
2018-01-20 00:31:07 +0200

    add connectionTab animation

---
+++ src/js/lib/connectionTab.js
@@ -0,0 +1,29 @@
+'use strict';
+var connectionTab = {
+  className: '.js-connection-tab',
+  clickableEl: '.connection__item',
+  imageClass: '.connection__image',
+  activeClass: 'connection__item' + '--active',
+  activeClassImages: 'connection__image' + '--active',
+  init: function(){
+    this.elemenets = $(this.className).find(this.clickableEl);
+    this.images = $(this.className).find(this.imageClass);
+    
+    this.elemenets.each((i,el)=>{
+      $(el).on('click', ()=>{this.changeTab(i)})
+    });
+  },
+  changeTab: function(index) {
+    this.deactiveAll();
+    this.activateTab(index);
+  },
+  activateTab: function(index){
+    this.elemenets.eq(index).addClass(this.activeClass);
+    this.images.eq(index).addClass(this.activeClassImages);
+  },
+  deactiveAll: function() {
+    this.elemenets.removeClass(this.activeClass);
+    this.images.removeClass(this.activeClassImages);
+  }
+}
+module.exports = connectionTab;
